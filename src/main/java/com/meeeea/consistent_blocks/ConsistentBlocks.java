74e640085897b4e6c2f7ae11dc6cb2d60b91b243
Justin Hutchens <Justinh22007@gmail.com>
2024-03-05 20:17:41 -0600

    modified some things where it will still run

--- src/main/java/com/meeeea/consistent_blocks/ConsistentBlocks.java
+++ src/main/java/com/meeeea/consistent_blocks/ConsistentBlocks.java
@@ -5,6 +5,8 @@ import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.minecraft.item.Item;
+
 public class ConsistentBlocks implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
