5293476dcea26a4e91ef40356be6b1280e055ec7
Justin Hutchens <Justinh22007@gmail.com>
2024-03-05 20:18:46 -0600

    more changes

--- src/main/java/com/meeeea/consistent_blocks/ConsistentBlocks.java
+++ src/main/java/com/meeeea/consistent_blocks/ConsistentBlocks.java
@@ -11,7 +11,7 @@ public class ConsistentBlocks implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("Consistent Blocks");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
